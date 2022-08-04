57abda1adbf860f26245028c405cb800c476c45c
arnabbiswas1 <arnab.biswas1@olacabs.com>
2022-08-04 12:32:25 +0530

    new change

--- pages/bucket.vue
+++ pages/bucket.vue
@@ -9,7 +9,7 @@
         </div>
     </nav>
     <div class="m-8">
-     <h6 align="center"><b>Please choose items to your bucket</b></h6>
+     <h6 align="center"><b><u>Please choose items to your bucket</u></b></h6>
      <div class="grid grid-cols-3 gap-5 mt-14">
       <div class="p-6 max-w-sm bg-red-800 rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 apple">
         <div class="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">{{apple.name}}</div>
@@ -153,12 +153,12 @@ export default {
 
 <style scoped>
 .apple{
-    background-color: red;
+    background-color: rgb(55, 0, 255);
 }
 .orange{
-    background-color: orange;
+    background-color: rgb(255, 0, 0);
 }
 .grapes{
-    background-color: blue;
+    background-color: rgb(200, 255, 0);
 }
 </style>
